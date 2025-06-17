17917016abb6a48d3bb8ab7828cb79707cc4bb40
Rahmanmaulani-code <144915469+Rahmanmaulani-code@users.noreply.github.com>
2025-06-17 15:27:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x xmrig
-./xmrig -o pool.scalaproject.io:3333 \
+./xmrig -o mine.scala.network:3333 \
         -u Ssy2SFtR3rGfjKFA4xxUnGVikpSy7GyfTFbDmjyqSGsuJMJ4K2GSKyK6pApbCy7DBGgf6b5p3fHwV1zHrnVRUjDG7kdm496A8R \
         -p x -a panthera -t 2
